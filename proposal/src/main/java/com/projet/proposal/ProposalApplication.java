ee0350732f6166e8b8e202b45992192187d5bfcf
Eugene Motolov <zheniamotolov@gmail.com>
2018-06-27 08:26:47 +0300

    first commit

---
+++ proposal/src/main/java/com/projet/proposal/ProposalApplication.java
@@ -0,0 +1,12 @@
+package com.projet.proposal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProposalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProposalApplication.class, args);
+	}
+}
